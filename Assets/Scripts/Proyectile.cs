60f29872288016c41016cc1f195d7bdafd15f0a0
ewong03 <ewong03@student.ubc.ca>
2024-08-19 11:18:22 -0500

    Initial Commit

---
+++ Assets/Scripts/Proyectile.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Proyectile : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    private bool hit;
+    private float direction;
+    private float lifetime;
+
+    private BoxCollider2D boxCollider;
+    private Animator anim;
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        boxCollider = GetComponent<BoxCollider2D>();
+        boxCollider.isTrigger = true;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (hit)return;
+        float movementSpeed = speed * Time.deltaTime * direction;
+        transform.Translate(movementSpeed,0,0);
+
+        lifetime += Time.deltaTime;
+        if (lifetime > 5) Deactivate();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        hit = true;
+        print("hi");
+        boxCollider.enabled = false;
+        anim.SetTrigger("explode");
+    }
+
+    public void SetDirection(float _direction)
+    {
+        lifetime= 0;
+        direction = _direction;
+        gameObject.SetActive(true);
+        hit = false;
+        boxCollider.enabled=true;
+
+        float localScaleX = transform.localScale.x;
+        if (Mathf.Sign(localScaleX) != _direction)
+            localScaleX = -localScaleX;
+
+        transform.localScale = new Vector3(localScaleX, transform.localScale.y, transform.localScale.z);
+    }
+
+    private void Deactivate()
+    {
+        gameObject.SetActive(false);
+    }
+}
