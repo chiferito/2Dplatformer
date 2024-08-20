267eb860109a2b69328399ea8c615205a0e7192c
ewong03 <ewong03@student.ubc.ca>
2024-08-20 12:42:18 -0500

    iframes

--- Assets/Scripts/Health/Health.cs
+++ Assets/Scripts/Health/Health.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class Health : MonoBehaviour
 {
+    [Header ("Health")]
     [SerializeField] private float startingHealth;
     public float currentHealth { get; private set; }
     private Animator anim;
     private bool dead;
 
+    [Header("iFrames")]
+    [SerializeField] private float iFrameDuration;
+    [SerializeField] private int numberOffFlashes;
+    private SpriteRenderer spriteRenderer;
+
     private void Awake()
     {
         currentHealth = startingHealth;
         anim = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     public void TakeDamage(float _damage)
@@ -23,7 +30,9 @@ public class Health : MonoBehaviour
         {
             //player hurt
             anim.SetTrigger("hurt");
+
             //iframes
+            StartCoroutine(Invulnerability());
         }
         else 
         {
@@ -41,6 +50,20 @@ public class Health : MonoBehaviour
     public void AddHealth(float _value)
     {
         currentHealth = Mathf.Clamp(currentHealth + _value, 0, startingHealth);
+    }
+
+    private IEnumerator Invulnerability()
+    {
+        Physics2D.IgnoreLayerCollision(8,9, true);
+        //invulnerable
+        for (int i = 0; i < numberOffFlashes; i++)
+        {
+            spriteRenderer.color = new Color(1, 0, 0, 0.5f);
+            yield return new WaitForSeconds(iFrameDuration/(numberOffFlashes*2));
+            spriteRenderer.color = Color.white;
+            yield return new WaitForSeconds(iFrameDuration / (numberOffFlashes * 2));
+        }
+        Physics2D.IgnoreLayerCollision(8, 9, false);
     }
 
     //public void Update() 
