0c2ab542ec4c92aef7cae37c60c5af7d3d09eb4a
ewong03 <ewong03@student.ubc.ca>
2024-08-19 21:09:25 -0500

    camera follow player

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -9,9 +9,19 @@ public class CameraController : MonoBehaviour
     private float currentPosX;
     private Vector3 velocity = Vector3.zero;
 
+    [SerializeField] private Transform player;
+    [SerializeField] private float aheadDistance;
+    [SerializeField] private float cameraSpeed;
+    private float lookAhead;
+
     private void Update()
     {
-        transform.position = Vector3.SmoothDamp(transform.position, new Vector3(currentPosX,transform.position.y,transform.position.z), ref velocity, speed);
+        //Room camera
+        //transform.position = Vector3.SmoothDamp(transform.position, new Vector3(currentPosX,transform.position.y,transform.position.z), ref velocity, speed);
+
+        //Follow player
+        transform.position = new Vector3(player.position.x + lookAhead,transform.position.y,transform.position.z);
+        lookAhead = Mathf.Lerp(lookAhead, (aheadDistance * player.localScale.x), Time.deltaTime * cameraSpeed);
     }
 
     public void MoveToNewRoom(Transform _newRoom)
